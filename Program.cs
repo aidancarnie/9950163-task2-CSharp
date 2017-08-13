b5cba35848cfd4ff52adf85f7490f15d5552f5f6
Aidan Carnie <9950163@BCS.NET.NZ>
2017-08-14 11:43:12 +1200

    Second Commit

--- Program.cs
+++ Program.cs
@@ -6,7 +6,24 @@ namespace _9950163_task2_CSharp
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //set variable for numbers
+            var num = 0;
+            
+            Console.WriteLine("This program will compute the average of 10 numbers");
+
+            for (var i = 0; i < num; i++)
+            {
+                //set calculation for average
+                var sum = num / 10;
+                //ask user to enter numbers
+                Console.WriteLine("Enter number 01: ");
+                //store the users input
+                Console.ReadLine();
+
+                //output the average of the numbers
+                Console.WriteLine("The average of those 10 numbers is: ", sum);
+            }
+            Console.ReadLine();
         }
     }
 }
